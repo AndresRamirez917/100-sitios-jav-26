d0877eca17302c05c834183ca1bfc2760e392bed
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-19 16:26:08 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -8,48 +8,25 @@ const buscar = (e) => {
     e.preventDefault();
     for(i = 0; i < loremSplit.length; i++){
         if(input_buscar.value == loremSplit[i]){
-            const card = document.createRange().createContextualFragment(`
-                
-                <p>${loremSplit[i]}</p>
-                
-                `)
-                const lorem = document.getElementById('lorem');
-                lorem.append(card)
-                lorem.textContent=loremSplit[i]
-                word = lorem.textContent=loremSplit[i]
-                madre.style.color="red"
-                if(madre.includes("Lorem")){
-                    console.log("madre")
+                console.log("debug")
+                if(madre.innerHTML = loremSplit[i]){
+                    madre.style.backgroundColor="red";
+                    madre.style.width="fit-content"
+                    return true;
                 }
-                return true;
             }
       }
       if(input_buscar.value != loremSplit[i]){
-        const card = document.createRange().createContextualFragment(`
+        const card2 = document.createRange().createContextualFragment(`
             
             <p>La palabra no existe</p>
             
             `)
             
             lorem.textContent=loremSplit[i]
-            lorem.append(card)
-           //lorem.remove(card)
+            lorem.append(card2)
     }
 }
 
 btn_buscar.addEventListener("click", buscar)
 
-function addLorem(){
-    const text = document.createRange().createContextualFragment(`
-        
-         
-                
-        
-        
-        `)
-        const lorem = document.getElementById('lorem');
-        lorem.append(text)
-    
-} 
-
-addLorem()
